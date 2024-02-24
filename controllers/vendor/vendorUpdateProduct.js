4882e60180aa6e831b330d98ad68ea22c9c25ecc
Ajeet Gupta <ajeetgupta@Ajeets-MacBook-Air.local>
2024-02-24 22:38:29 +0530

    variant update sucessfully

--- controllers/vendor/vendorUpdateProduct.js
+++ controllers/vendor/vendorUpdateProduct.js
@@ -3,13 +3,13 @@ const db = require('../../config/index')
 exports.vendorUpdateProduct = async (req, res, next) =>{
     try {
         const {vendor_shop_id} = req.vendor;
-        const {product_id} = req.query;
+        const {variant_id} = req.query;
         const updates = Object.keys(req.body)
             .map(key => `${key} = '${req.body[key]}'`)
             .join(', ');
 
-        const result = await db.sequelize.query('DECLARE @success BIT;EXEC UpdateProductColumn :product_id , :shop_id, @updates = :updates, @success = @success OUTPUT; Select @success as success', {
-            replacements: { product_id: product_id, shop_id: vendor_shop_id, updates },
+        const result = await db.sequelize.query('DECLARE @success BIT;EXEC UpdateProductColumn :variant_id , :shop_id, @updates = :updates, @success = @success OUTPUT; Select @success as success', {
+            replacements: { variant_id: variant_id, shop_id: vendor_shop_id, updates },
             type: db.Sequelize.QueryTypes.SELECT
         });
 
