ea0372901bbea8392f47e3d53190cd88f8c6db4d
Ajeet Gupta <ajeetgupta@Ajeets-MacBook-Air.local>
2024-02-26 22:22:53 +0530

    user apis for category and subcategory

--- controllers/user/userFetchShop.js
+++ controllers/user/userFetchShop.js
@@ -29,25 +29,29 @@ const db = require('../../config/index')
 
 exports.userFetchShop = async (req, res, next) => {
     try {
-        const { pincode, latitude, longitude } = req.query;
+        const { pincode, latitude, longitude } = req.query
 
-        const result = await db.sequelize.query("EXEC GetNearestShops @pincode = :pincode, @lat = :lat, @lon = :lon", {
-            replacements: {
-                pincode: pincode,
-                lat: latitude,
-                lon: longitude
-            },
-            type: db.sequelize.QueryTypes.SELECT 
-        });
-        
-        return res.json(result); 
+        const result = await db.sequelize.query(
+            'EXEC GetNearestShops @pincode = :pincode, @lat = :lat, @lon = :lon',
+            {
+                replacements: {
+                    pincode: pincode,
+                    lat: latitude,
+                    lon: longitude,
+                },
+                type: db.sequelize.QueryTypes.SELECT,
+            }
+        )
 
+        return res
+            .status(200)
+            .json({ shopID: result[0]?.ShopID, success: true, error: false })
     } catch (error) {
-        console.log(error);
+        console.log(error)
         return res.status(500).json({
             message: error.message,
             success: false,
-            error: true
+            error: true,
         })
     }
 }
--- routes/user/userRoutes.js
+++ routes/user/userRoutes.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const { userFetchShop } = require('../../controllers/user/userFetchShop');
+const { getCategoryForShop } = require('../../controllers/user/getCategoryForShop');
+const { getSubcategoriesForCategories } = require('../../controllers/user/getSubcategoriesForCategories');
 
 const router = express.Router();
 
 
 
 router.get('/get-nearest-shop', userFetchShop);
+
+router.get('/get-product-category/:vendor_shop_id', getCategoryForShop)
+
+router.get('/get-subcategory-of-categories', getSubcategoriesForCategories);
 
 
 module.exports = router;
